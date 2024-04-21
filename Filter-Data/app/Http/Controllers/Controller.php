a2c986a70e8807ed1d3e6c5aa98c3b86139abc70
Suwit1100 <suwitlichoeiwara@gmail.com>
2024-04-21 16:23:15 +0700

    สร้าง route controller ui

--- Filter-Data/app/Http/Controllers/Controller.php
+++ Filter-Data/app/Http/Controllers/Controller.php
@@ -4,5 +4,8 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function index()
+    {
+        return view('index');
+    }
 }
--- Filter-Data/routes/web.php
+++ Filter-Data/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\FilterDataController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [FilterDataController::class, 'index'])->name('index');
