45c56716420296a27a41af6253130ea5831b46ac
Suwit1100 <suwitlichoeiwara@gmail.com>
2024-04-22 12:36:37 +0700

    สร้าง  paginator

--- Filter-Data/app/Http/Controllers/FilterDataController.php
+++ Filter-Data/app/Http/Controllers/FilterDataController.php
@@ -2,12 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\Pagination\Paginator;
 use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Pagination\Paginator as PaginationPaginator;
+use Illuminate\Support\Facades\Http;
 
 class FilterDataController extends Controller
 {
     public function index()
     {
-        return view('index');
+        PaginationPaginator::useBootstrap();
+        $testid = "24";
+        $response = Http::get('https://6624b23204457d4aaf9cc0f6.mockapi.io/api/ApiMockFSW/ProductMock');
+        $product = collect($response->json());
+
+        $filteredProduct = $product->when($testid, function ($query, $testid) {
+            return $query->where('id', "$testid");
+        });
+
+        // จำนวนรายการที่ต้องการแสดงต่อหน้า
+        $perPage = 8;
+
+        if ($filteredProduct->count() > 0) {
+            // หาหน้าปัจจุบัน
+            $currentPage = LengthAwarePaginator::resolveCurrentPage();
+            // คำนวณ offset สำหรับข้อมูลที่จะแสดงในหน้าปัจจุบัน
+            $currentPageItems = $filteredProduct->slice(($currentPage - 1) * $perPage, $perPage)->all();
+
+            // สร้าง paginator instance โดยใช้ LengthAwarePaginator
+            $paginator = new LengthAwarePaginator($currentPageItems, $filteredProduct->count(), $perPage);
+
+            // กำหนด URL ของหน้าต่อไปและหน้าก่อนหน้า
+            $paginator->setPath(url()->current());
+
+            // dd($paginator);
+
+            // ส่ง paginator instance ไปใช้งานใน view
+            return view('index', ['paginator' => $paginator]);
+        } else {
+            // ถ้าไม่มีข้อมูลให้ส่งข้อมูลว่างๆ ไปยัง view
+            return view('index', ['paginator' => null]);
+        }
     }
 }
--- Filter-Data/resources/views/index.blade.php
+++ Filter-Data/resources/views/index.blade.php
@@ -62,6 +62,21 @@
             </div>
         </div>
     </div>
+    {{-- {{ dd($paginator) }} --}}
+    @if ($paginator)
+        <!-- แสดงข้อมูลที่ได้รับ -->
+        @foreach ($paginator->items() as $item)
+            <!-- นำข้อมูล $item ไปใช้งาน -->
+            {{ $item['id'] }}
+        @endforeach
+
+        <!-- แสดงลิงก์หน้า pagination -->
+        {{ $paginator->links() }}
+    @else
+        <!-- ถ้าไม่มีข้อมูล -->
+        หน้า 1
+    @endif
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
     </script>
